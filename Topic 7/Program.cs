e448a191d313c087bc43e1a1e3cfa745d6bf04b6
Noah Hopkins <noah.mich.hopk@gmail.com>
2023-04-05 10:54:37 -0400

    just need to do 5

--- Topic 7/Program.cs
+++ Topic 7/Program.cs
@@ -1,4 +1,6 @@
 ﻿using static System.Formats.Asn1.AsnWriter;
+using System.Collections.Generic;
+
 
 namespace Topic_7
 {
@@ -7,9 +9,9 @@ namespace Topic_7
         static void Main(string[] args)
         {
             string input;
-            int inputNum, listSum, listAverage, listMin, listMax;
+            int inputNum, listSum, listAverage, listMin, listMax, numSearch, removeNum, addNum;
             int listLength = 25;
-            int[] list = new int[listLength];
+            List<int> numbers = new List<int>();
             Random generator = new Random();
 
             Console.WriteLine("Before we begin what should be the lowest and highest possible number on the list?");
@@ -18,10 +20,9 @@ namespace Topic_7
             Console.WriteLine("List Maximum: ");
             listMax = Convert.ToInt32(Console.ReadLine());     
 
-            for (int listNum = 0; listNum < list.Length;)
+            for (int listNum = 0; listNum < listLength; listNum++)
             {
-                list[listNum] = generator.Next(listMin, listMax);
-                listNum++;
+                 numbers.Add(generator.Next(listMin, listMax));          
             }
 
 
@@ -34,106 +35,141 @@ namespace Topic_7
                 Console.WriteLine("5 - Count the number of occurences of a specific number");
                 Console.WriteLine("6 - Print the largest value");
                 Console.WriteLine("7 - Print the smallest value");
-                Console.WriteLine("8 - Get the average of the numbers in the list");
+                Console.WriteLine("8 - Get the sum and the average of the numbers in the list");
                 Console.WriteLine("9 - Make a new list of random numbers with a new minimum and maximum");
                 Console.WriteLine("10 - Quit");
                 Console.WriteLine();
                 input = Console.ReadLine();
-                Console.WriteLine();
 
                 if (Int32.TryParse(input, out inputNum))
                 {
+                    Console.Clear();
+                    Thread.Sleep(250);
                     if (inputNum == 1)
                     {
-                        Array.Sort(list);
-                        foreach (int i in list)
+                        numbers.Sort();
+                        foreach (int i in numbers)
                         {
-                            Console.Write(i + " ");
+                            Console.Write(i + ", ");
                         }
                         Console.WriteLine();
                         Console.WriteLine();
-                        Thread.Sleep(250);
                     }
                     else if (inputNum == 2)
-                    {                  
-                        for (int listNum = 0; listNum < list.Length;)
+                    {
+                        Thread.Sleep(250);
+                        Console.Clear();
+                        for (int listNum = 0; listNum < listLength;)
                         {
-                            list[listNum] = generator.Next(listMin, listMax);
+                            numbers[listNum] = generator.Next(listMin, listMax);
                             listNum++;
                         }
-                        foreach (int i in list)
+                        foreach (int i in numbers)
                         {
-                            Console.Write(i + " ");
+                            Console.Write(i + ", ");
                         }
                         Console.WriteLine();
                         Console.WriteLine();
-                        Thread.Sleep(250);
+
                     }
                     else if (inputNum == 3)
                     {
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        Console.Clear();
                         Thread.Sleep(250);
+                        foreach (int i in numbers)
+                        {
+                            Console.Write(i + ", ");
+                        }
+                        Console.WriteLine(); 
+                        Console.WriteLine("Enter the number: ");
+                        removeNum = Convert.ToInt32(Console.ReadLine());
+                        numbers.RemoveAll(x => x == removeNum);
+                        foreach (int i in numbers)
+                        {
+                            Console.Write(i + " ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine();
 
                     }
                     else if (inputNum == 4)
                     {
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        Console.Clear();
                         Thread.Sleep(250);
+                        foreach (int i in numbers)
+                        {
+                            Console.Write(i + ", ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("Enter the number: ");
+                        addNum = Convert.ToInt32(Console.ReadLine());
+                        numbers.Insert(0, addNum);
+                        foreach (int i in numbers)
+                        {
+                            Console.Write(i + ", ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine();
 
                     }
                     else if (inputNum == 5)
                     {
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        Console.Clear();
                         Thread.Sleep(250);
+                        Console.WriteLine("Enter number:");
+                        numSearch = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine();
 
                     }
                     else if (inputNum == 6)
                     {
-                        Console.WriteLine(list.Max());
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        Console.Clear();
                         Thread.Sleep(250);
+                        Console.WriteLine(numbers.Max());
+                        Console.WriteLine();
+                        Console.WriteLine();
 
                     }
                     else if (inputNum == 7)
                     {
-                        Console.WriteLine(list.Min());
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        Console.Clear();
                         Thread.Sleep(250);
+                        Console.WriteLine(numbers.Min());
+                        Console.WriteLine();
+                        Console.WriteLine();
 
                     }
                     else if (inputNum == 8)
                     {
-                        listSum = list.Sum();
+                        Console.Clear();
+                        listSum = numbers.Sum();
+                        Console.WriteLine("The sum of the list is " + listSum);
                         listAverage = listSum / listLength;
-                        Console.WriteLine(listAverage);
+                        Console.WriteLine("The average of the list is " + listAverage);
                         Console.WriteLine();
                         Console.WriteLine();
-                        Thread.Sleep(250);
                     }
                     else if (inputNum == 9)
                     {
+                        Console.Clear();
+                        Thread.Sleep(250);
                         Console.WriteLine("List Minimum: ");
                         listMin = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("List Maximum: ");
                         listMax = Convert.ToInt32(Console.ReadLine());
                       
-                        for (int listNum = 0; listNum < list.Length;)
+                        for (int listNum = 0; listNum < listLength;)
                         {
-                            list[listNum] = generator.Next(listMin, listMax);
+                            numbers[listNum] = generator.Next(listMin, listMax);
                             listNum++;
                         }
-                        foreach (int i in list)
+                        foreach (int i in numbers)
                         {
-                            Console.Write(i + " ");
+                            Console.Write(i + ", ");
                         }
                         Console.WriteLine();
                         Console.WriteLine();
-                        Thread.Sleep(250);
                     }
                     else if (inputNum == 10)
                     {
@@ -142,7 +178,7 @@ namespace Topic_7
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Try again."); 
+                    Console.WriteLine("Invalid input. Try again.");
                     Thread.Sleep(250);
                 }
 
